f881e22d55365fed80a7fdc424ef65af3278d709
Alikhan Tokenov <alikhantokenov@gmail.com>
2026-02-21 13:03:41 +0500

    Worker module refactoring

--- src/erl_py_runner_worker.erl
+++ src/erl_py_runner_worker.erl
@@ -25,7 +25,7 @@
 start_child(Number, Config) ->
   {ok, PID} =
     supervisor:start_child(erl_py_runner_worker_sup, #{
-      id => get_id(Number),
+      id => new_worker_id(Number),
       start => {erl_py_runner_worker, start, [Config]},
       restart => permanent,
       shutdown => 60000,
@@ -37,6 +37,7 @@ start(Config) ->
   PID =
     spawn_link(
       fun() ->
+        % We need this flag to be able to close the port before process shutdown.
         process_flag(trap_exit, true),
         State = init(Config),
         erl_py_runner_pool:send_worker_start(self()),
@@ -61,9 +62,9 @@ do_run(Worker, Data, Timeout) ->
     {complete, Worker, Output} ->
       erlang:demonitor(Ref, [flush]),
       {ok, Output};
-    {error, Worker, Reason} ->
+    {error, Worker, Error} ->
       erlang:demonitor(Ref, [flush]),
-      {error, Reason};
+      {error, Error};
     {'DOWN', Ref, process, Worker, Reason} ->
       {error, Reason}
   after Timeout ->
@@ -83,10 +84,17 @@ init(#{
       [{packet, 4}, binary, exit_status]
     ),
   init_port(Port, Timeout, AllowedPythonModules),
+  
+  
   #state{
     port = Port,
     timeout = Timeout,
-    allowed_modules = AllowedErlangModules
+    allowed_modules =
+      case AllowedErlangModules of
+        Modules when is_list(Modules) -> maps:from_keys(AllowedErlangModules, true);
+        all -> all;
+        _Other -> #{}
+      end
   }.
 
 init_port(Port, Timeout, AllowedModules) ->
@@ -101,10 +109,17 @@ init_port(Port, Timeout, AllowedModules) ->
       case erlang:binary_to_term(Response) of
         #{status := ?CALL_STATUS_OK} ->
           ok;
-        #{status := ?CALL_STATUS_ERROR, error := Reason} ->
+        #{status := ?CALL_STATUS_ERROR, error := Error} ->
+          ?LOGERROR("received error from port during initialization: ~p, error: ~p", [
+            Port,
+            Error
+          ]),
           erlang:port_close(Port),
-          exit({initialization_fail, Reason})
-      end
+          exit({initialization_fail, Error})
+      end;
+    {Port, {exit_status, StatusCode}} ->
+      ?LOGERROR("received exit from port: ~p, status code: ~p", [Port, StatusCode]),
+      exit({port_exit, {status_code, StatusCode}})
   after
     Timeout ->
       erlang:port_close(Port),
@@ -114,40 +129,40 @@ init_port(Port, Timeout, AllowedModules) ->
 loop(#state{port = Port} = State) ->
   receive
     {run, Caller, Data} ->
-      case send_data(State, Data) of
+      case send_data(Data, State) of
         {ok, Output} ->
-          Caller ! {complete, self(), Output};
+          send_complete(Caller, Output);
         {error, Reason} ->
           ?LOGERROR("failed send data to port: ~p, caller: ~p, reason: ~p", [
             Port,
             Caller,
             Reason
           ]),
-          Caller ! {error, self(), Reason}
+          send_error(Caller, Reason)
       end,
       erl_py_runner_pool:send_worker_ready(self()),
       loop(State);
-    {'EXIT', Port, Reason} ->
-      ?LOGERROR("received exit from port, reason: ~p", [Reason]),
-      exit(Reason);
+    {Port, {exit_status, StatusCode}} ->
+      ?LOGERROR("received exit from port: ~p, status code: ~p", [Port, StatusCode]),
+      exit({port_exit, {status_code, StatusCode}});
     {'EXIT', From, Reason} ->
       ?LOGERROR("received exit from: ~p, reason: ~p", [From, Reason]),
       erlang:port_close(Port),
       exit(Reason);
     Unexpected ->
-      ?LOGWARNING("received unexpected message: ~p", [Unexpected]),
+      ?LOGWARNING("received unexpected message: ~p, ignored", [Unexpected]),
       loop(State)
   end.
   
 send_data(
-  #state{port = Port} = State,
-  #{code := _, arguments := _} = Inputs
+  #{code := _, arguments := _} = Inputs,
+  #state{port = Port} = State
 ) ->
-  case erlang:port_command(Port, erlang:term_to_binary(Inputs), [nosuspend]) of
-    true -> wait_data(State);
-    false -> {error, aborted}
+  case send_command(Port, Inputs) of
+    ok -> wait_data(State);
+    error -> {error, aborted}
   end;
-send_data(_State, _Inputs) ->
+send_data(_Inputs, _State) ->
   {error, invalid_inputs}.
 
 wait_data(#state{port = Port, timeout = Timeout} = State) ->
@@ -160,14 +175,14 @@ wait_data(#state{port = Port, timeout = Timeout} = State) ->
         Response ->
           {ok, Response}
       end;
-    {'EXIT', Port, Reason} ->
-      ?LOGERROR("received exit from port: ~p, reason: ~p", [Port, Reason]),
-      exit(Reason)
+    {Port, {exit_status, StatusCode}} ->
+      ?LOGERROR("received exit from port: ~p, status code: ~p", [Port, StatusCode]),
+      exit({port_exit, {status_code, StatusCode}})
   after
     Timeout ->
       ?LOGERROR("port timeout after ~p ms, closing port: ~p", [Timeout, Port]),
       erlang:port_close(Port),
-      exit(timeout)
+      exit(port_timeout)
   end.
 
 %%% +--------------------------------------------------------------+
@@ -212,18 +227,30 @@ handle_call(
           error => iolist_to_binary(io_lib:format("~p", [Error]))
         }
     end,
-  erlang:port_command(Port, erlang:term_to_binary(Response), [nosuspend]).
+  send_command(Port, Response).
+
+new_worker_id(Number) ->
+  erlang:list_to_atom(?WORKER_NAME ++ erlang:integer_to_list(Number)).
+  
+send_complete(PID, Output) ->
+  PID ! {complete, self(), Output}.
+  
+send_error(PID, Error) ->
+  PID ! {error, self(), Error}.
+
+send_command(Port, Data) ->
+  case erlang:port_command(Port, erlang:term_to_binary(Data), [nosuspend]) of
+    true -> ok;
+    false -> exit(port_command)
+  end.
 
 check_module(_Module, all) ->
   ok;
 check_module(Module, AllowedModules) ->
-  case lists:member(Module, AllowedModules) of
-    true ->
+  case AllowedModules of
+    #{Module := true} ->
       ok;
-    false ->
+    _Other ->
       ErrorMessage = iolist_to_binary(io_lib:format("module ~ts is not allowed", [Module])),
       throw({not_allowed, ErrorMessage})
   end.
-
-get_id(Index) ->
-  erlang:list_to_atom(?WORKER_NAME ++ erlang:integer_to_list(Index)).
